34ddfadd7d9fc64242f1eca2e31a8fb18a23848a
Sofia-Ami <89886838+Sofia-Ami@users.noreply.github.com>
2021-09-21 17:52:08 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: 'unlock',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'Sei Fushimi',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: 'Kou Takahashi',
           // q2: 'かかか',
           // q3: 'ききき',
         }
@@ -99,8 +99,8 @@ app.component('answer-input', {
   data: function () {
     return {
       /* 送信ボタン上下に表示されるメッセージ */
-      okMessage: '正解！',
-      ngMessage: 'そのキーワードは違うようだぞ！？',
+      okMessage: '正解だ',
+      ngMessage: '-ERROR-どうやら違うらしい',
       message: '',
       inputAnswer: '',
     }
